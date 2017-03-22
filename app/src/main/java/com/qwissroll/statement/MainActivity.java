a6dba8cf5559cfb0e1c2a7cb6c035dc970d6d27c
roll <quek.ruoling@gmail.com>
2017-03-22 23:52:10 +0800

    Remove unused imports

--- app/src/main/java/com/qwissroll/statement/MainActivity.java
+++ app/src/main/java/com/qwissroll/statement/MainActivity.java
@@ -1,14 +1,7 @@
 package com.qwissroll.statement;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
