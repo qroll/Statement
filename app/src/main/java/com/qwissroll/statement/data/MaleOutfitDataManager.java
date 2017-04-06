03deb547b0ea4d19f291877a8129a3b890d47c02
roll <quek.ruoling@gmail.com>
2017-04-06 10:26:49 +0800

    Update products for male fashion

--- app/src/main/java/com/qwissroll/statement/data/MaleOutfitDataManager.java
+++ app/src/main/java/com/qwissroll/statement/data/MaleOutfitDataManager.java
@@ -16,16 +16,21 @@ public class MaleOutfitDataManager extends OutfitDataManager {
     protected MaleOutfitDataManager() {
         dashboardItems = new LinkedHashMap<>();
 
-        DashboardItem item1 = new DashboardItem(10, "Prom Day", false,
-                new ArrayList<>(Arrays.asList("formal", "bowtie", "long sleeved", "monochrome")));
+        DashboardItem item1 = new DashboardItem(11, "Classic", false,
+                new ArrayList<>(Arrays.asList("jeans", "casual", "tshirt", "basic")));
         item1.setLikes(7);
-        dashboardItems.put(10, item1);
+        dashboardItems.put(11, item1);
 
-        DashboardItem item2 = new DashboardItem(5, "Black is the new black", false,
+        DashboardItem item2 = new DashboardItem(10, "Fedora man", false,
+                new ArrayList<>(Arrays.asList("fedora", "hat", "bowtie", "casual", "long sleeved", "monochrome")));
+        item2.setLikes(7);
+        dashboardItems.put(10, item2);
+
+        DashboardItem item3 = new DashboardItem(5, "Blacker black", false,
                 new ArrayList<>(Arrays.asList("casual", "jacket", "jeans", "long sleeved", "denim", "black", "monochrome")));
-        item2.addComment(new Comment("Jonathan", "You look like a celebrity :)"));
-        item2.setLikes(2);
-        dashboardItems.put(5, item2);
+        item3.addComment(new Comment("Jonathan", "You look like a celebrity :)"));
+        item3.setLikes(2);
+        dashboardItems.put(5, item3);
     }
 
     public static OutfitDataManager getInstance() {
--- app/src/main/java/com/qwissroll/statement/data/MaleProductDataManager.java
+++ app/src/main/java/com/qwissroll/statement/data/MaleProductDataManager.java
@@ -15,14 +15,19 @@ public class MaleProductDataManager extends ProductDataManager {
         items = new LinkedHashMap<Integer, ArrayList<DetailItem>>();
 
         ArrayList<DetailItem> first = new ArrayList<DetailItem>();
-        first.add(new DetailItem(15, "White button-up shirt", "", false));
+        first.add(new DetailItem(10, "White t-shirt", "", false));
         first.add(new DetailItem(23, "Blue jeans", "", false));
-        items.put(10, first);
+        items.put(11, first);
 
         ArrayList<DetailItem> second = new ArrayList<DetailItem>();
         second.add(new DetailItem(31, "T-shirt", "", false));
         second.add(new DetailItem(32, "Black jeans", "", false));
         items.put(5, second);
+
+        ArrayList<DetailItem> third = new ArrayList<DetailItem>();
+        third.add(new DetailItem(33, "Fedora", "", false));
+        third.add(new DetailItem(15, "White button-up shirt", "", false));
+        items.put(10, third);
     }
 
     public static ProductDataManager getInstance() {
