bb5e7d48873aa831b74cb460dcf7a0de7c414ee6
roll <quek.ruoling@gmail.com>
2017-04-07 12:27:57 +0800

    Fix scrolling not being detected outside of item list

--- app/src/main/java/com/qwissroll/statement/view/TagInputView.java
+++ app/src/main/java/com/qwissroll/statement/view/TagInputView.java
@@ -2,7 +2,6 @@ package com.qwissroll.statement.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.inputmethod.EditorInfo;
